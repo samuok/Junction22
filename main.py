4442f635c70370386b019913e740586ac2313136
samuok <samu.oksala@hotmail.com>
2022-11-04 22:08:51 +0200

    test

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import mariadb
+import pandas
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sqlalchemy
+from sqlalchemy import create_engine
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def connect():
+    # Connect to MariaDB Platform
+    try:
+        conn = mariadb.connect(
+            user="kayttis",
+            password="salis",
+            host="localhost",
+            port=3306,
+            database="junction"
+
+        )
+
+        print('Success')
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+    # Get Cursor
+    cur = conn.cursor()
+
+    # cur.execute(
+    #   "INSERT INTO junction (arvo,arvo1, arvo2, arvo3, arvo4) VALUES (?, ?, ?, ?, ?)",
+    #  (4, 5, 8, 1, 5))
+
+    #cur.execute("SELECT * FROM junction")
+
+    engine = sqlalchemy.create_engine("mariadb+mariadbconnector://kayttis:salis@127.0.0.1:3306/junction")
+
+
+    df = pandas.read_sql("SELECT * FROM junction", engine)
+    #conn.commit()
+
+    print(df)
+
+
+
+    #print(q)
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    connect()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
