cdf83f508f9e61b4346041c29df3a1df7fe05371
jussi <jussimristolainen@gmail.com>
2022-11-04 21:40:16 +0200

    Made Ui

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Ui(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        loadUi("mainscreen.ui",self)
+        self.Answer1.valueChanged.connect(self.setValue1)
+        self.Answer2.valueChanged.connect(self.setValue2)
+        self.Answer3.valueChanged.connect(self.setValue3)
+        self.Answer4.valueChanged.connect(self.setValue4)
+        self.Submit.clicked.connect(self.loadtoDataBase)
 
+    def setValue1(self, item):
+        self.Value1.setText(str(item))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def setValue2(self, item):
+        self.Value2.setText(str(item))
 
+    def setValue3(self, item):
+        self.Value3.setText(str(item))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def setValue4(self, item):
+        self.Value4.setText(str(item))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def loadtoDataBase(self):
+        print("hello")
+
+app = QApplication([])
+window = Ui()
+window.show()
+app.exec_()
+
